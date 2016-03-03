1b0c67b5d5f9222edc868da4fccb16d866926006
jwzimmer1990 <jwzimmer1990@gmail.com>
2016-03-03 22:42:24 +0200

    add montecarlopi file

---
+++ montecarlopi.py
@@ -0,0 +1,13 @@
+#i did some monte carlo simulations in college but have since forgotten a lot and want to make sure i can still
+#understand the concepts : )
+
+
+def montecarlopi(datapointsinsimulation, radius=1):
+    """take the radius of a circle, which will also be the side of (1/4 of) the square,
+    and the amount of data wanted to be in the simulation
+    note: will look at 1/4 of the square and circle because it's easier"""
+    #pi will come from the ratio of the square's area and circle's area:
+    #circle = (pi*r^2)/4
+    #square = (r^2)/4
+    #since r != 0 --> pi = circle/ (pi*r^2/4) = circle/ square
+    return None
